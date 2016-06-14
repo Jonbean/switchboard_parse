43693c1ed3213064293fda37c6d112f70621eb80
Jon <jonczbean@gmail.com>
2016-06-14 10:12:51 -0500

    split data into three sets

--- parsing_all.sh
+++ parsing_all.sh
@@ -5,3 +5,8 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
     echo "parsing $line"
     python parsing_all.py $line > "./result/$line"
 done < "$1"
+
+mkdir train val test
+mv sw{2005-4000} ./train
+mv sw{4003-4153} ./val
+mv sw{4154-4484} ./test
